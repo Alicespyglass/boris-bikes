5f8706eba8efbeae9819745eacd92907743f5802
alicespyglass <alicespyglass@gmail.com>
2017-03-16 10:45:47 +0000

    13 - exceptions for docking station and bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,12 +4,15 @@ class DockingStation
 attr_reader :bike
 
   def release_bike
-    Bike.new
+    fail 'No bikes available' unless @bike
+    @bike
   end
 
   def dock(bike)
+    fail 'Docking Station Full!' if @bike
     @bike = bike
   end
+
 
 =begin This is what attr_reader does - this is a getter
   def bike
--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -3,9 +3,26 @@ require 'docking_station.rb'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
 
-  it 'releases working bikes' do
-    bike = subject.release_bike # sets up test to create subject
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    describe '#release_bike' do
+    it 'raises an error when bike method called on docking station equals nil' do
+      expect {subject.release_bike}.to raise_error "No bikes available"
+    end
+
+    describe '#dock' do
+      it 'raises an error when a bike is attempted to dock in a docking station with 1 bike in.' do
+        subject.dock(Bike.new)
+        expect {subject.dock(Bike.new)}.to raise_error "Docking Station Full!"
+      end
+    end
+  end
+
   end
 
   # docking_station.dock(bike)
