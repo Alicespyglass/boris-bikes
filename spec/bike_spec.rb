ef9642e9bdf9790ff16e9a513d632aa1a87c3ea6
alicespyglass <alicespyglass@gmail.com>
2017-03-16 14:54:26 +0000

    18 report broken bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,4 +3,9 @@ require './lib/bike.rb'
 describe Bikes do
   it { is_expected.to respond_to :working? }
 
+  it 'Can be reported broken' do
+    subject.report_broken
+    expect(subject).to be_broken
+  end
+
 end
