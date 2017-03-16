5aaac4218e0fc9ac4a8c9ca068c182c7cfe2ffbe
alicespyglass <alicespyglass@gmail.com>
2017-03-16 11:48:18 +0000

    15 - SRP full and empty

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,19 +8,31 @@ attr_reader :bike
     @bikes = []
   end
 
+
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
 # Rename your attribute @bike to reflect that it will store more than one Bike instance.
 
   def dock(bike)
-    fail 'Docking Station Full!' if @bikes.count >= 20
+    fail 'Docking Station Full!' if full?
     @bikes.push(bike) # or <<
   end
 
+  #  Define a full? predicate method that uses some of the dock code
+  # to return true or false depending on whether the station is full or not
 
+private
+
+    def full?
+      @bikes.count >= 20
+    end
+
+    def empty?
+     @bikes.empty?
+    end
 
 =begin This is what attr_reader does - this is a getter
   def bike
